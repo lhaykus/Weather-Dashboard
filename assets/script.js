18e62cc304af6161abbcc63b122b739269675868
Loryn <lorynhaykus@yahoo.com>
2021-05-29 22:20:53 -0600

    got search history to show up

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
 //start function
+APIkey = "862f97705f5cae105644a854f96037eb";
+let city = $("#city-search").val();
+
 $(document).ready(function () {
-    // let APIkey = "862f97705f5cae105644a854f96037eb";
-    //Creating click event for the search button 
+    let cityHistory = [];
+ 
+
+function saveCities() {
+    let searches = [];
+    cities = Object.keys(localStorage),
+    i = cities.lenght;
+    while (i -- ) {
+        searches.push(localStorage.getItem(cities[i]));
+    }
+    for (j=0; j < searches.length; j++) {
+        $("#search-history-results").append("<button class='city-searches>" + searches[j] + "</button>");
+    }
+ }
+ saveCities();
+ listCities();
+
+ function listCities () {
+    let listItem = $("<li>").addClass("list-group-item").text(city);
+    $("#history-list").append(listItem);
+    saveCities();
+}
     $('#searchBtn').click(function () {
         //creating a variable for the value of the input = city
         let city = $("#city-search").val();
@@ -19,22 +42,48 @@ $(document).ready(function () {
                     $("#showCurrentWeather").html(current);
                     //Empty the search bar when button is pushed
                     $("#city-search").val('');
+                    $("#search-history-results").append("<button class='city-history'>" + city + "</button>");
+                    localStorage.setItem(city, city);
+
 
                 }
             })
-        }
+        } $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + "&APPID=862f97705f5cae105644a854f96037eb",
+            type: "GET",
+        }).then(function (response) {
+            for (let i =0; i < response.list.lenght; i++ ) {
+                let dateTime = response.list[i].dt_txt;
+                let date = dateTime.split(" ")[0];
+                let time = dateTime.split(" ")[1];
 
-    });
+                if (time === "12:00:00") {
+                    $("#day-" + day_counter).children(".card-date").text(month + "/" + day + "/" + year);
+                    $("#day-" + day_counter).children(".icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
+                    $("#day-" + day_counter).children(".5day-temp").text("Temperature: " + response.list[i].main.temp + "°F");
+                    $("#day-" + day_counter).children(".5day-humidity").text("Humidity: " +response.list[i].main.humidity + "%");
+                    day_counter++;
+                    
+                }
+            }
+        })
+        })
+        });
+      //  getFutureForecast();
+       
     //Created function to get data for current weather
     function showCurrentWeather(data) {
         //Creating h1 with data.name to display the name of the current city searched
-        return "<h1 stlye='font-size: 50px; font-weight: bold;'> " + data.name + " </h1>" +
+     return "<h1 stlye='font-size: 50px; font-weight: bold;'> " + data.name + " </h1>" +
+        //Creating an image to display the weather icon that matches the data.weather
             "<img src=' https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png'> " +
         //Creating an h2 element to display the description data from the weather object with index 0
-        "<h3>Condition: " + data.weather[0].main + "</h3>" +
+             "<h3>Condition: " + data.weather[0].main + "</h3>" +
             //Displaying the temp that is found in the main object
             "<h3>Temperature: " + data.main.temp + "°F </h3>" +
+            //Displaying the humidity 
             "<h3>Humidity: " + data.main.humidity + "% </h3>" +
+            //Displaying the wind speed 
             "<h3>Wind Speed: " + data.wind.speed + "MPH </h3>" 
             
     }
@@ -42,7 +91,12 @@ $(document).ready(function () {
 
 
 
-})
+
+
+
+
+
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Weather Dashboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Sources
+
+This YouTube video really helped me out with AJAX and API calls and walking me through the steps
+https://www.youtube.com/watch?v=6imiFFeDIzE 
