0574d938cca16391b8ee4cf5ae5ef4875114d921
Loryn <lorynhaykus@yahoo.com>
2021-05-30 19:12:54 -0600

    have current weather and local storage working trying to figure out 5day and uv

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,43 @@
-//start function
+//Declaring variables
 APIkey = "862f97705f5cae105644a854f96037eb";
 let city = $("#city-search").val();
+let longitude;
+let latitude;
 
+//start function
 $(document).ready(function () {
+
+  //Function to save searches to local storage
+  function saveCities() {
+    //creating an empty array to store the searched cities in
+    let searches = [];
+    //
+    cities = Object.keys(localStorage),
+        i = cities.lenght;
+    while (i--) {
+        //pushing searches into local storage and getting the stores cities from local storage index
+        searches.push(localStorage.getItem(cities[i]));
+    }
+    for (j = 0; j < searches.length; j++) {
+        //Creating buttons for each search that is saved in search history
+        $("#search-history-results").append("<button class='city-searches>" + searches[j] + "</button>");
+    }
+};
+
+//function to save to local storage
+saveCities();
+//function to list the search history
+listCities();
+
+//Function to list the search history citites
+function listCities() {
+    //adding <li> element to the html class and giving them the text of the name of the city that was searched
+    let listItem = $("<li>").addClass("list-group-item").text(city);
+    //appending the list items to the id history-list to show on the page
+    $("#history-list").append(listItem);
+    saveCities();
+};
+
 
 
     //Function for when the search button is clicked
@@ -16,8 +51,7 @@ $(document).ready(function () {
             //Call function to call the API for current weather
             getCurrentWeather();
             getForecast();
-
-
+            getUV();
 
         }
 
@@ -29,128 +63,88 @@ $(document).ready(function () {
                 type: "GET",
             }).then(function (data) {
                 console.log(data);
-                    //Create variable to use showCurrentWeather function
-                    let current = showCurrentWeather(data);
-                    //Displaying the value to the html id of showCurrentWeather
-                    $("#showCurrentWeather").html(current);
-                    //Appending the search history and creating buttons with the name of the city searched
-                    $("#search-history-results").append("<button class='city-history'>" + city + "</button>");
-                    //saving city into local storage
-                    localStorage.setItem(city, city);
-
+                //Create variable to use showCurrentWeather function
+                let current = showCurrentWeather(data);
+                //Displaying the value to the html id of showCurrentWeather
+                $("#showCurrentWeather").html(current);
+                //Appending the search history and creating buttons with the name of the city searched
+                $("#search-history-results").append("<button class='city-history'>" + city + "</button>");
+                //saving city into local storage
+                localStorage.setItem(city, city);       
 
             });
-         }
-            
-   
-
-            //Created function to get data for current weather
-            function showCurrentWeather(data) {
-                //Creating h1 with data.name to display the name of the current city searched
-                return "<h1 stlye='font-size: 50px; font-weight: bold;'> " + data.name + " </h1>" +
-                    //Creating an image to display the weather icon that matches the data.weather
-                    "<img src=' https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png'> " +
-                    //Creating an h2 element to display the description data from the weather object with index 0
-                    "<h3>Condition: " + data.weather[0].main + "</h3>" +
-                    //Displaying the temp that is found in the main object
-                    "<h3>Temperature: " + data.main.temp + "°F </h3>" +
-                    //Displaying the humidity 
-                    "<h3>Humidity: " + data.main.humidity + "% </h3>" +
-                    //Displaying the wind speed 
-                    "<h3>Wind Speed: " + data.wind.speed + "MPH </h3>"
-
-            };
-        
+        };
 
 
 
-         function getForecast(list) {
-          
-            $.ajax({
-                url:'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial&APPID=862f97705f5cae105644a854f96037eb",
-                type: "GET",
-             //   dataType: "jsonp"
-            }).then(function (list) {
-                console.log(list);
-                let forecast = showForecast(list);
-               $("#showForecast").html(forecast);
-                //Empty out cards to show next forecast when new city is searched
-                    $("#showForecast").empty();
-           
-        });
-            function showForecast(data) {
-                  //Loop through 5 days
-                  for (let i= 0; i < data.lenght[i].length; i++) {
-                   //  return   "<img src=' https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png'> " +
-                   //    "<h3>Temperature: " + data.list[i].main.temp + "°F </h3>" +
-                      //Displaying the humidity 
-                   //   "<h3>Humidity: " + data.list[i].main.humidity + "% </h3>" 
-                   let card = $("<div>");
-                    let title =$("<h2>");
-                    let cardCol = $("<div class='col-2'>");
-                    let cardImg = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png ");
-                    let cardTemp = $("<h3>").text("Temperature: " + list[i].main.temp + " °F");
-                    let cardHumid = $("<h3>").text("Humidity: " + list[i].main.humidity + " %");
+        //Created function to get data for current weather
+        function showCurrentWeather(data) {
+            //Creating h1 with data.name to display the name of the current city searched
+            return "<h1 stlye='font-size: 50px; font-weight: bold;'> " + data.name + " </h1>" +
+                //Creating an image to display the weather icon that matches the data.weather
+                "<img src=' https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png'> " +
+                //Creating an h2 element to display the description data from the weather object with index 0
+                "<h3>Condition: " + data.weather[0].main + "</h3>" +
+                //Displaying the temp that is found in the main object
+                "<h3>Temperature: " + data.main.temp + "°F </h3>" +
+                //Displaying the humidity 
+                "<h3>Humidity: " + data.main.humidity + "% </h3>" +
+                //Displaying the wind speed 
+                "<h3>Wind Speed: " + data.wind.speed + "MPH </h3>"
 
-                   $("#showForecast").append(card.append(cardCol.append((title).text(moment().add(i, "day").format('L')),cardImg, cardTemp, cardHumid)));
-
-
-        
-           
-
-
-                    };
                 
-
-              };
-              
-            
-            }
-    
-            })
-                   
-
-          
-
-                  
-
-        //Function to save searches to local storage
-        function saveCities() {
-            //creating an empty array to store the searched cities in
-            let searches = [];
-            //
-            cities = Object.keys(localStorage),
-                i = cities.lenght;
-            while (i--) {
-                //pushing searches into local storage and getting the stores cities from local storage index
-                searches.push(localStorage.getItem(cities[i]));
-            }
-            for (j = 0; j < searches.length; j++) {
-                //Creating buttons for each search that is saved in search history
-                $("#search-history-results").append("<button class='city-searches>" + searches[j] + "</button>");
-            }
-        };
-        
-        //function to save to local storage
-        saveCities();
-        //function to list the search history
-        listCities();
-
-        //Function to list the search history citites
-        function listCities() {
-            //adding <li> element to the html class and giving them the text of the name of the city that was searched
-            let listItem = $("<li>").addClass("list-group-item").text(city);
-            //appending the list items to the id history-list to show on the page
-            $("#history-list").append(listItem);
-            saveCities();
         };
 
         
-        
-       
-    
 
-         
-       
-             })
-            
+function getUV(data) {
+    latitude = data.coord.lat;
+    longitude = data.coord.lon;
+    
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' +latitude + "&lon=" +longitude + "&units=imperial&APPID=862f97705f5cae105644a854f96037eb",
+        type: "GET",
+    }).then(function(response){
+        let currentUV = $("#uv-index");
+        currentUV.append($("#showCurrentWeather").text("UV Index: ") + response.value);
+    });
+};
+
+
+        function getForecast() {
+           
+            $.ajax({           
+                url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial&APPID=862f97705f5cae105644a854f96037eb",
+                type: "GET",
+                 dataType: "json",
+                 success: function(response) {
+                    console.log(response);
+
+                    let results = response.list;
+                    console.log(results);
+
+                    
+    
+            //        let forecast = showForecast(results);
+           //         $("#showForecast").html(forecast);
+
+
+                    for (let i = 0; i < results[i].lenght; i+=8) {
+                        console.log(results[i].dt_txt);
+
+                    return  "<img src=' https://openweathermap.org/img/wn/" + results.weather[0].icon + ".png'> " +
+                         "<h3>Temperature: " + results.main.temp + "°F </h3>" +
+                             //Displaying the humidity 
+                             "<h3>Humidity: " + results.main.humidity + "% </h3>"
+
+                 }
+           
+
+                     },
+
+        });
+
+        }
+    })
+
+})
